6bc65c726014fa2c352218a871963585da651ef3
Yudai-Nakano-web <yudai508god@icloud.com>
2020-02-24 18:05:30 +0900

    展示場一覧、展示場カード　作成途中

---
+++ src/components/HallCard.vue
@@ -0,0 +1,44 @@
+<template>
+  <section class="card">
+    <img class="img" v-bind:src="hallImage" alt="" />
+    <div class="card-content">
+      <h1 class="title">{{ hallTitle }}</h1>
+      <p class="name">{{ ownerName }}</p>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  props: [
+    //各データはどう用意する？
+    hallImage,
+    hallTitle,
+    ownerName
+  ]
+};
+</script>
+
+<style>
+.card {
+  width: 300px;
+  background: #fff;
+}
+.img {
+  border-radius: 5px 5px 0 0;
+  max-width: 100%;
+  height: auto;
+}
+.card-content {
+  padding: 20px;
+}
+.title {
+  font-size: 14px;
+  color: #333;
+}
+.name {
+  color: #777;
+  font-size: 12px;
+  line-height: 1.5;
+}
+</style>
---
+++ src/views/HallList.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="container">
+    <!-- v-forでカードのコンポーネントを繰り返し使う　カードはどうやって用意する？-->
+    <hall-card> </hall-card>
+  </div>
+</template>
+
+<script src="https://unpkg.com/magic-grid/dist/magic-grid.min.js"></script>
+<script>
+import HallCard from "../components/HallCard.vue";
+
+export default {
+  components: {
+    HallCard
+  }
+};
+
+//レイアウト用のマジックグリッドはここに記述？
+let magicGrid = new MagicGrid({
+  container: ".container",
+  animate: true,
+  gutter: 30,
+  static: true,
+  useMin: true
+});
+magicGrid.listen();
+</script>
