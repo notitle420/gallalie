237eb4a0fc5e006c00d44cf9d2ee4f93f39742b3
notitle.420@gmail.com <notitle.420@gmail.com>
2020-03-08 18:19:51 +0900

    fix double initial firebase

--- src/main.js
+++ src/main.js
@@ -3,22 +3,9 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify';
-import firebase from 'firebase'
 
 Vue.config.productionTip = false
 
-
-const config = {
-　　 apiKey: "AIzaSyAuGjc-ciG6HK8qEgJRre9IGc9pZqipnUM",
-    authDomain: "gallalie-staging.firebaseapp.com",
-    databaseURL: "https://gallalie-staging.firebaseio.com",
-    projectId: "gallalie-staging",
-    storageBucket: "gallalie-staging.appspot.com",
-    messagingSenderId: "152453665449",
-    appId: "1:152453665449:web:98a15a96393e0b575a4fb8",
-};
-
-firebase.initializeApp(config);
 
 new Vue({
   router,
